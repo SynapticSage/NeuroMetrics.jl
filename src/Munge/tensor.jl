fe87f129882a0751d39dbc6b55296f5a0cc38711
Ryan <ryoung@brandeis.edu>
2022-07-13 14:59:20 -0400

    tensor creation works

--- src/Munge/tensor.jl
+++ src/Munge/tensor.jl
@@ -13,6 +13,8 @@ module tensor
     using StatsBase
     using Statistics
     using Infiltrator
+    using ProgressMeter
+    using Missings
 
     import Utils
 
@@ -26,7 +28,7 @@ module tensor
 
     create a tensor of a point process arrayed with some set of N-dimensions
     """
-    function tensor_pointproc(X::DataFrame)
+    function tensor_pointproc(X::DataFrame)::Array
     end
 
     """
@@ -45,24 +47,11 @@ module tensor
      
     """
     function tensor_continuous(X::DataFrame, dims::Vector, var::T where T<:SymStr;
-            quantilebin::Dict{<:SymStr, <:Int}=Dict{Symbol,Int}(),
-            relative::Union{Vector{<:SymStr},Nothing}=nothing,
-            TensType::Type=Float64)
+            TensType::Type=Float64)::Array
 
         X = copy(X)
-        
-        # Clean
-        X = dropmissing(X, [dims...])
-        notisnan = hcat(Utils.notisnan.(eachcol(X[!,dims]))...)
-        notisnan = Utils.squeeze(all(notisnan, dims=2))
-        X = X[notisnan, :]
+        X = clean(X, dims)
 
-        if isempty(quantilebin) != true
-            X = quantilize(X, quantilebin)
-        end
-        if relative !== nothing
-            X = relativize(X, dims, relative)
-        end
         G = groupby(X[!, [dims..., var]], dims)
         counts = combine(G, nrow)
         if !(all(counts.nrow .== 1))
@@ -73,40 +62,69 @@ module tensor
         end
 
         axs = OrderedDict(dim=>sort(unique(X[!,dim])) for dim in dims)
-        sz   = [length(ax) for ax in axs]
-        T = Array{TensType}(undef,sz...)
-        @infiltrate
-        for g in G
-            searches = map((dim, ax) -> first(g)[dim] .== ax, zip(dims, axs))
-            overall = (accumulate(.&, searches))[end]
-            for i = 1:length(searches)
-                searches[i] .&= overall
+        sz   = [length(ax) for (_,ax) in axs]
+        TensType = TensType !== DataFrame ? 
+           Array{Union{Missing,TensType}}(missing,sz...) :
+           Union{Missing,DataFrame}
+
+        @showprogress for g in G
+            # Lookup the dim values of g in the axs lists
+            function match_func(dim, ax)
+                first(g)[dim] .== ax
             end
-            indices = Tuple(findfirst(search) for search in searches)
-            T[indices...] = singular ? g[!, var] : g[!, var][1]
+            searches = map(match_func, keys(axs),values(axs))
+            #overall = accumulate(.&, searches)[end]
+            #for i = 1:length(searches)
+            #    searches[i] .&= overall
+            #end
+            indices = findfirst.(searches)
+            T[indices...] = singular ? g[!, var][1] : Array(g[!, var])
         end
+        T
     end
 
+    """
+        equalize
+
+    equalizes dimensions -- the block will have missing values where no samples
+    are had. this attempts to make a core dense block with no missing values by
+    throwing out values from slices, so all slices have equal samples and no
+    missing values.
+    """
     function equalize
     end
 
-    function quantilize(X::DataFrame, quantilebin::Dict{<:SymStr, <:Int},)
+    function quantilize(X::DataFrame, dims::Vector{<:SymStr},
+            quantilebin::Dict{<:SymStr, <:Int},)::DataFrame
+        X = clean(X, dims)
         # Rank bin anything requested
         for (var, bins) in quantilebin
             quant = X[!,var]./maximum(X[!,var])
             binned = ceil.((bins-1)*quant) 
             X[!,var] = binned
         end
+        X
     end
 
-    function relativize(X::DataFrame, dims::Vector{<:SymStr}, var::Vector{<:SymStr}; keeporig::Bool=false)
+    function relativize(X::DataFrame, dims::Vector{<:SymStr},
+            var::Vector{<:SymStr}; keeporig::Bool=false)::DataFrame
+        X = clean(X, dims)
         # Rank bin anything requested
         makerelative(x) = x .- minimum(x)
         pos = keeporig ? [v => identity => Symbol("orig"*String(v)) for v in var] : []
         combine(groupby(X, dims, sort=false), var .=> makerelative .=> var, pos...)
     end
-    function relativize(X::DataFrame, dims::Vector{<:SymStr}, var::T where  T<:SymStr; keeporig::Bool=false)
+    function relativize(X::DataFrame, dims::Vector{<:SymStr}, var::T where
+            T<:SymStr; keeporig::Bool=false)::DataFrame
         relativize(X, dims, [var]; keeporig)
+    end
+
+    function clean(X::DataFrame, dims::Vector{<:SymStr}=All())::DataFrame
+        # Clean
+        X = dropmissing(X, [dims...])
+        notisnan = hcat(Utils.notisnan.(eachcol(X[!,dims]))...)
+        notisnan = Utils.squeeze(all(notisnan, dims=2))
+        X = X[notisnan, :]
     end
 
 end
