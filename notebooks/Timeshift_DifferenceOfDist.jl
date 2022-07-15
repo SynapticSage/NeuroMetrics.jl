59d6fa69e3c938d8c026e58ed73c1586253bcc53
Ryan <ryoung@brandeis.edu>
2022-07-15 17:58:22 -0400

    Name change and more plots for difference of distributions

--- notebooks/Timeshift_DifferenceOfDist.jl
+++ notebooks/Timeshift_DifferenceOfDist.jl
@@ -67,6 +67,14 @@ end
 # ╔═╡ 94816c70-20ab-4e80-93fb-ed1ce0b3405a
 PlutoUI.TableOfContents(title="Comparing field shifts")
 
+# ╔═╡ d38b9928-8654-4e9d-9671-3341e19b9b95
+md"""
+#### Comparing field shifts by difference
+Purpose: To compare field shifts by taking the difference of two datasets per neuron
+
+##### *TODOs*
+"""
+
 # ╔═╡ 6a1d85d7-f98e-4af5-aa8d-0f9696b8bad8
 md"""
 # Preamble
@@ -115,7 +123,12 @@ datacut = [datacut₁..., datacut₂...]
 @bind area PlutoUI.Radio(["CA1-PFC", String.(unique(I.area))...], default="CA1-PFC")
 
 # ╔═╡ 4c670dcd-25ff-43d1-b78f-2b60fc5878d5
+begin
+	name₁ = "$(datacut[1])"
+	name₂ = "$(datacut[2])"
+inv_comparison_name = "Δ $(datacut[2])-$(datacut[1])"
 comparison_name = "Δ $(datacut[1])-$(datacut[2])"
+end
 
 # ╔═╡ 48c22294-fc9d-44ff-b2ea-b2c59e795da0
 datacut
@@ -153,21 +166,74 @@ begin
 		append!(dfDiff, c)
 	end
 	dfDiff=Timeshift.operation.add_best_shift(dfDiff);
+	dfDiff=Timeshift.operation.add_worst_shift(dfDiff);
 	dfDiff=Timeshift.operation.add_centroid_shift(dfDiff);
 	dfDiff
 end;
 
+# ╔═╡ 59cc0e98-4a9d-428f-bb4e-f61568491c4b
+md"""
+# Plots
+
+## Δ (🔥Heatmaps) and sort(🔥Heatmaps)
+"""
+
+# ╔═╡ cebd71af-7dd1-48cd-9fa6-eb16c7499a4a
+md"""
+Layout *TODO*
+  * Row 1: 1-2 (sorted by best), 1 sorted by best, 2 sort by the same
+  * Row 1: 2-1 (sorted by best), 1 sorted by sa,e, 2 sort by the best
+"""
+
 # ╔═╡ a7727c6b-b284-4862-876d-591051ce5a2d
-with_logger(NullLogger()) do
-h_diff = heatmap(Float32.(replace(
-	Matrix(
-		sort(unstack(Timeshift.norm_information(dfDiff), [:unit, :bestshift], :shift, :value)[:,Not(:unit)], :bestshift)
-	), missing=>NaN)
-), clim=(0.25,1))
+begin
+	function unstack_to_heatmap(H; sortby=:bestshift)
+	H = sort(unstack(Timeshift.norm_information(H, minmax=[-1,1]), [:unit, sortby], :shift, :value), sortby)
+		x,y = H.unit, names(H[:,Not([:unit, sortby])])
+		H = Float32.(replace(Matrix(H[:, Not([:unit, sortby])]), missing=>NaN))
+		parse.(Float64,y),x,H
+	end
+
+	Hα = [unstack_to_heatmap(dfDiff)...]
+	Hβ = [unstack_to_heatmap(dfDiff; sortby=:worstshift)...]
+	plot(
+		heatmap(Hα[1],1:length(Hα[2]),Hα[3]; yticks=[], xlabel="Shift", ylabel="Neuron", title=String(name₁), colorbar_position=:left),
+		heatmap(Hβ[1],1:length(Hβ[2]),Hβ[3]; xlabel="Shift", ylabel="", title=String(name₂), yticks=[], colorbar_title="$comparison_name", cb=:bottom)
+	)
 end
 
+# ╔═╡ bce5a58c-da1e-4942-96fa-e6e825597b18
+Markdown.LaTeX
+
+# ╔═╡ 2201dfaf-cc57-4a61-91ee-4ebf971bd5ad
+md"""
+## Distribution of best_shift(distribution)
+"""
+
+# ╔═╡ 7de3ac0c-84d0-4da3-b0f9-91ca1687c2d0
+begin
+	ly = Plots.@layout [a [b; c]]
+	a = histogram2d(dfDiff.bestshift, dfDiff.worstshift, xlabel="$name₁", ylabel="$name₂")
+	scatter!(dfDiff.bestshift, dfDiff.worstshift, xlabel="$name₁", ylabel="$name₂", label="", aspect_ratio=1, markeralpha=0.008, markerstrokecolor=:black, markerstrokewidth=1)
+	plot!(-4:1:4, -4:1:4, c=:black, linestyle=:dash, linewidth=2)
+
+	b = histogram(dfDiff.bestshift - dfDiff.worstshift, xlabel="typical shift of $comparison_name")
+	c = histogram(dfDiff.worstshift - dfDiff.bestshift, xlabel="typical shift of $inv_comparison_name")
+
+	Plots.plot(a,b,c; layout=ly)
+end
+
+# ╔═╡ a277f8bf-fd19-44e6-a452-e502880eccd2
+plotattr(:Series)
+
 # ╔═╡ ee2ce4a3-f36f-4814-9853-1dddf1984b56
-datasets
+datacut
+
+# ╔═╡ d5344211-1c10-4a99-846a-9443f81f5d08
+md"""
+## Distribution of `normalized_difference/shift`
+TODO : We could have two ways of normalized differences. One would be taking the difference in the raw and then normalizing to [-1,1]. Another way (this way) is first normalizing information (in the two distributions) and then taking the difference.
+"""
 
 # ╔═╡ 11704bde-db87-4469-97cf-e2a4fc5c4c76
 begin
@@ -188,23 +254,31 @@ end
 
 # ╔═╡ Cell order:
 # ╟─94816c70-20ab-4e80-93fb-ed1ce0b3405a
+# ╟─d38b9928-8654-4e9d-9671-3341e19b9b95
 # ╟─6a1d85d7-f98e-4af5-aa8d-0f9696b8bad8
 # ╟─ef3ed745-3204-494b-bd4a-f0c849255f2e
 # ╟─f9d33aee-5392-41a6-bf03-fd9244adfa0f
 # ╟─81dd0cb9-5400-4502-8a27-0530d730c616
 # ╟─4d85a1f8-fe28-45bd-970c-cab78838034f
-# ╟─2ab79319-1f6f-431f-93ab-dcb5c381afd8
+# ╠═2ab79319-1f6f-431f-93ab-dcb5c381afd8
 # ╟─68f79249-c6f2-4f54-b3b6-e0559f9bb7b5
 # ╟─49896b0c-19d3-4c51-8e54-2f046e6d8382
-# ╟─e8221624-f6fd-11ec-0151-cf9da8e7d639
-# ╟─948387de-df9e-4dde-b616-4fa54e13738d
-# ╟─b38a32ff-9b95-416c-be82-3d7371d51932
-# ╟─b741339a-8a3b-416d-9a59-33624595f56f
-# ╟─4c670dcd-25ff-43d1-b78f-2b60fc5878d5
-# ╟─48c22294-fc9d-44ff-b2ea-b2c59e795da0
+# ╠═e8221624-f6fd-11ec-0151-cf9da8e7d639
+# ╠═948387de-df9e-4dde-b616-4fa54e13738d
+# ╠═b38a32ff-9b95-416c-be82-3d7371d51932
+# ╠═b741339a-8a3b-416d-9a59-33624595f56f
+# ╠═4c670dcd-25ff-43d1-b78f-2b60fc5878d5
+# ╠═48c22294-fc9d-44ff-b2ea-b2c59e795da0
 # ╠═f5817112-3b0a-43a4-b3ae-28c94be89580
 # ╠═6c0bc43e-5f22-4007-8f00-317f67f3e31b
 # ╠═61f417ed-a4c9-42a3-b858-17dc170dfea0
-# ╠═a7727c6b-b284-4862-876d-591051ce5a2d
+# ╟─59cc0e98-4a9d-428f-bb4e-f61568491c4b
+# ╟─cebd71af-7dd1-48cd-9fa6-eb16c7499a4a
+# ╟─a7727c6b-b284-4862-876d-591051ce5a2d
+# ╠═bce5a58c-da1e-4942-96fa-e6e825597b18
+# ╟─2201dfaf-cc57-4a61-91ee-4ebf971bd5ad
+# ╟─7de3ac0c-84d0-4da3-b0f9-91ca1687c2d0
+# ╠═a277f8bf-fd19-44e6-a452-e502880eccd2
 # ╟─ee2ce4a3-f36f-4814-9853-1dddf1984b56
-# ╠═11704bde-db87-4469-97cf-e2a4fc5c4c76
+# ╟─d5344211-1c10-4a99-846a-9443f81f5d08
+# ╟─11704bde-db87-4469-97cf-e2a4fc5c4c76
