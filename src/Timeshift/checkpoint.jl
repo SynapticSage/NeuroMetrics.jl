716c4800ec702a88dcf933331d4acd264adda43b
Ryan <ryoung@brandeis.edu>
2022-07-31 23:20:29 -0400

    Simplified boilerplate calls.

--- src/Timeshift/checkpoint.jl
+++ src/Timeshift/checkpoint.jl
@@ -48,10 +48,27 @@ module checkpoint
         Load.save_cell_taginfo(df_imax, animal, day, tag)
     end
 
-    function mainspath()
+    function storepath(store::String)
         parent_folder = datadir("exp_pro", "timeshift")
-        name = joinpath(parent_folder, "mains")
+        joinpath(parent_folder, store)
     end
+    mainspath()    = storepath("mains")
+    shufflespath() = storepath("shuffles")
+    fieldspath()   = storepath("fields")
+
+    function load_store(store; dataframe::Bool=false)
+        if dataframe
+            name = storepath(store) * "_dataframe.arrow"
+            DataFrame(Arrow.Table(name))
+        else
+            name = storepath(store)
+            D = deserialize(name)
+        end
+    end
+    load_mains(;kws...)    = load_store("mains"; kws...)
+    load_shuffles(;kws...) = load_store("shuffles"; kws...)
+    load_fields(;kws...)   = load_store("fields"; kws...)
+
 
     """
     old = OrderedCollections.OrderedDict{Float64, Dict{Any(NamedTuple), Any}} 
@@ -104,15 +121,7 @@ module checkpoint
         end
         R
     end
-    function load_mains(;dataframe::Bool=false)
-        if dataframe
-            name = mainspath() * "_dataframe.arrow"
-            DataFrame(Arrow.Table(name))
-        else
-            name = mainspath()
-            D = deserialize(name)
-        end
-    end
+    
 
 
     function save_fields(M::AbstractDict; overwrite::Bool=false)
@@ -134,30 +143,12 @@ module checkpoint
         name = fieldspath() * "_dataframe.arrow"
         Arrow.write(name, F)
     end
-    function fieldspath()
-        parent_folder = datadir("exp_pro", "timeshift")
-        joinpath(parent_folder, "fields")
-    end
-    function load_fields(;dataframe::Bool=false)
-        if dataframe
-            name = fieldspath() * "_dataframe.arrow"
-            DataFrame(Arrow.Table(name))
-        else
-            name = fieldspath()
-            deserialize(name)
-        end
-    end
 
     function shufflefieldspath()
         parent_folder = datadir("exp_pro", "timeshift")
         joinpath(parent_folder, "shufflefields")
     end
 
-
-    function shufflespath()
-        parent_folder = datadir("exp_pro", "timeshift")
-        joinpath(parent_folder, "shuffles")
-    end
     function save_shufflefields(S::AbstractDict; overwrite::Bool=false,
         num_examples::Int=3)
         name = shufflefieldspath()
@@ -214,99 +205,6 @@ module checkpoint
         Arrow.write(name, S)
     end
 
-    function load_shuffles(;dataframe::Bool=false)
-        if dataframe
-            name = shufflespath() * "_dataframe.arrow"
-            DataFrame(Arrow.Table(name))
-        else
-            name = shufflespath()
-            deserialize(name)
-        end
-    end
-
-    # ===================
-    # Old Ways
-    # ==================
-    """
-    _pathshiftdat
-
-    provides path for saved data
-
-    TODO make more flexible
-    """
-    function _pathshiftdat(;metric=nothing, shifts=nothing, 
-                tag="", props=nothing, splitby=nothing, kws...)
-
-            if props isa Nothing
-                @error "field keyword props must be given"
-            end
-            if splitby isa Nothing
-                @error "field keyword splitby must be given"
-            end
-
-            parent_folder = datadir("exp_pro", "timeshift")
-            if !(isdir(parent_folder))
-                mkdir(parent_folder)
-            end
-
-            tag = isempty(tag) ? tag : "_$tag"
-            if shifts !== nothing
-                start, stop = round(shifts[begin],digits=3),
-                              round(shifts[end],  digits=3)
-                N = length(shifts)
-                shifts = "_Nstartstop=($N,$start:$stop)"
-            else
-                @error "No shifts name provided"
-            end
-            if metric === nothing
-                @warn "No metric name provided, assuming metric='field'"
-                metric = "field"
-            end
-            jf(x) = join(x,'-')
-            props   = "props=$(jf(props))"
-            splitby = "_splitby=$(jf(splitby))"
-            joinpath(parent_folder, "$props$splitby$shifts$tag.serial")
-        end
-    function saveshifts(main=nothing, shuffle=nothing; overwrite=false, kws...)
-
-        name = _pathshiftdat(;kws...)
-        if isfile(name)
-            D = deserialize(name)
-        else
-            D = Dict()
-        end
-
-        kws = Dict(zip(keys(kws), values(kws)))
-
-        # Remove parts that will break serialization
-        if :filters ∈ propertynames(kws)
-            pop!(kws, :filters)
-        end
-        if :metric ∈ kws
-            metric = pop!(kws, :metric)
-        else
-            metric = nothing
-        end
-        if :shifts ∈ kws
-            shifts = pop!(kws, :shifts)
-        else
-            @error "Provide shifts"
-        end
-
-        d = Dict(:main     => main,
-                 :shuffle  => shuffle,
-                 :shifts   => shifts,
-                 :metric   => metric,
-                 :fieldkws => kws)
-        D = overwrite ? merge(D, d) : merge(d, D)
-        serialize(name, D)
-    end
-    function loadshifts(;kws...)::Dict
-        name = _pathshiftdat(;kws...)
-        println(name)
-        deserialize(name)
-    end
-
     function archivepath(store::String; archive=".archive")::String
         pathfunc = eval(Symbol(store * "path"))
         path = pathfunc()
@@ -315,21 +213,26 @@ module checkpoint
 
     function archive(store::String, keysearch::NamedTuple; archive=".archive", overwrite::Bool=false)
         pathfunc = eval(Symbol(store * "path"))
-        path = pathfunc()
-        archivepath = path * archive
-        store, archive = deserialize(path), 
+        storepath = pathfunc()
+        archivepath = storepath * archive
+        store, archive = deserialize(storepath), 
                          overwrite ? OrderedDict() : deserialize(archivepath)
         matches = match(keys(store), keysearch)
-        @info "Matches" matches
-        for key in matches
-            @info "Archiving" key
-            push!(archive, pop!(store, key))
-        end
         @infiltrate
-        @info("Serializing archive")
-        serialize(archivepath, archive)
-        @info("Serializing store")
-        serialize(archivepath, store)
+        if !(isempty(matches))
+            @info "Matches" matches
+            for key in matches
+                @info "Archiving" key
+                push!(archive, key=> pop!(store, key))
+            end
+            @infiltrate
+            @info("Serializing archive")
+            serialize(archivepath, archive)
+            @info("Serializing store")
+            serialize(storepath, store)
+        else
+            @info "No matches"
+        end
         nothing
     end
     function unarchive(store::String, keysearch::NamedTuple; archive=".archive")
@@ -353,6 +256,11 @@ module checkpoint
 
     end
     
+    function match(store::String, search::NamedTuple)
+        loadfunc = eval(Symbol("load_" * store))
+        I = loadfunc()
+        match(I, search)
+    end
     function match(dict_w_ntkeys::AbstractDict, search::NamedTuple)
         match(keys(dict_w_ntkeys), search)
     end
@@ -373,5 +281,87 @@ module checkpoint
     end
 
 
+    # ===================
+    # Old Ways
+    # ==================
+    #"""
+    #_pathshiftdat
+
+    #provides path for saved data
+
+    #TODO make more flexible
+    #"""
+    #function _pathshiftdat(;metric=nothing, shifts=nothing, 
+    #            tag="", props=nothing, splitby=nothing, kws...)
+
+    #        if props isa Nothing
+    #            @error "field keyword props must be given"
+    #        end
+    #        if splitby isa Nothing
+    #            @error "field keyword splitby must be given"
+    #        end
+
+    #        parent_folder = datadir("exp_pro", "timeshift")
+    #        if !(isdir(parent_folder))
+    #            mkdir(parent_folder)
+    #        end
+
+    #        tag = isempty(tag) ? tag : "_$tag"
+    #        if shifts !== nothing
+    #            start, stop = round(shifts[begin],digits=3),
+    #                          round(shifts[end],  digits=3)
+    #            N = length(shifts)
+    #            shifts = "_Nstartstop=($N,$start:$stop)"
+    #        else
+    #            @error "No shifts name provided"
+    #        end
+    #        if metric === nothing
+    #            @warn "No metric name provided, assuming metric='field'"
+    #            metric = "field"
+    #        end
+    #        jf(x) = join(x,'-')
+    #        props   = "props=$(jf(props))"
+    #        splitby = "_splitby=$(jf(splitby))"
+    #        joinpath(parent_folder, "$props$splitby$shifts$tag.serial")
+    #    end
+    #function saveshifts(main=nothing, shuffle=nothing; overwrite=false, kws...)
+
+    #    name = _pathshiftdat(;kws...)
+    #    if isfile(name)
+    #        D = deserialize(name)
+    #    else
+    #        D = Dict()
+    #    end
+
+    #    kws = Dict(zip(keys(kws), values(kws)))
+
+    #    # Remove parts that will break serialization
+    #    if :filters ∈ propertynames(kws)
+    #        pop!(kws, :filters)
+    #    end
+    #    if :metric ∈ kws
+    #        metric = pop!(kws, :metric)
+    #    else
+    #        metric = nothing
+    #    end
+    #    if :shifts ∈ kws
+    #        shifts = pop!(kws, :shifts)
+    #    else
+    #        @error "Provide shifts"
+    #    end
+
+    #    d = Dict(:main     => main,
+    #             :shuffle  => shuffle,
+    #             :shifts   => shifts,
+    #             :metric   => metric,
+    #             :fieldkws => kws)
+    #    D = overwrite ? merge(D, d) : merge(d, D)
+    #    serialize(name, D)
+    #end
+    #function loadshifts(;kws...)::Dict
+    #    name = _pathshiftdat(;kws...)
+    #    println(name)
+    #    deserialize(name)
+    #end
 
 end
