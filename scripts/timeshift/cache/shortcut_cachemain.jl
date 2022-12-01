0593e868182f5f0729bc8e5201edfbce0b2aa734
Ryan <ryoung@brandeis.edu>
2022-12-01 13:51:23 -0500

    Adapted script that caches shifted fields to do isolated spike shifted fields

--- scripts/timeshift/cache/shortcut_cachemain.jl
+++ scripts/timeshift/cache/shortcut_cachemain.jl
@@ -26,6 +26,8 @@ import Load
 import Filt
 import Munge
 filts = Filt.get_filters_precache()
+#get_shortcutnames(items)  = [replace(item, recon_process.var_shortcut_names...)
+#                         for item in items]
 
 # source:  https://github.com/fonsp/Pluto.jl/issues/115  
 function ingredients(path::String)
@@ -50,27 +52,27 @@ function get_key(;shifts, kws...)
     first=first(shifts), last=last(shifts), 
                step=Float64(shifts.step)) 
 end
-function keymessage(I::AbstractDict, key)
-    @info key
-    docontinue=false
-    if Utils.namedtup.orderlessmatch(key, keys(I))
-        if I[key] isa Task && !(istaskfailed(I[key]))
-            #@info "task key=$key already exists"
-            printstyled("SKIPPING...\n", blink=true)
-            docontinue=true
-        elseif I[key] isa Task && istaskfailed(I[key])
-            "key=$key already exists, but failed...redo!"
-        else
-            #@info "key=$key already exists"
-            printstyled("SKIPPING...\n", blink=true)
-            docontinue=true
-        end
-    end
-    if key ∉ keys(I)
-        #@info "key=$key ∉ keys, ...creating..."
-    end
-    docontinue
- end
+#function keymessage(I::AbstractDict, key)
+#    @info key
+#    docontinue=false
+#    if Utils.namedtup.orderlessmatch(key, keys(I))
+#        if I[key] isa Task && !(istaskfailed(I[key]))
+#            #@info "task key=$key already exists"
+#            printstyled("SKIPPING...\n", blink=true)
+#            docontinue=true
+#        elseif I[key] isa Task && istaskfailed(I[key])
+#            "key=$key already exists, but failed...redo!"
+#        else
+#            #@info "key=$key already exists"
+#            printstyled("SKIPPING...\n", blink=true)
+#            docontinue=true
+#        end
+#    end
+#    if key ∉ keys(I)
+#        #@info "key=$key ∉ keys, ...creating..."
+#    end
+#    docontinue
+#end
  PROPS = ["x", "y", "currentHeadEgoAngle", "currentPathLength", "stopWell"]
  IDEALSIZE = Dict(key => (key=="stopWell" ? 5 : 40) for key in PROPS)
  widths = 5.0f0
@@ -83,10 +85,27 @@ shifts = shifts .* 2f0
 I = OrderedDict{NamedTuple, Any}()
 F = OrderedDict{NamedTuple, Any}()
 
+(animal, day, frac) = first((("RY16",36, :iso),))
 @showprogress "animal" for (animal, day, frac) in (("RY16",36, :iso),) #Load.animal_set
     #animal, day = "RY16", 36, 
     @time spikes, beh, ripples, cells = Load.load(animal, day);
     _, spikes = Load.register(beh, spikes; transfer=["velVec"], on="time")
+    lfp = Load.load_lfp(animal, day, tet=:default, subtract_earlytime=true)
+
+    if frac == :iso
+        Munge.lfp.annotate_cycles(lfp::DataFrame; 
+                                  phase_col="phase", 
+                                  method="peak-to-peak")
+        histogram(lfp.time); histogram!(spikes.time)
+        spikes = Munge.spiking.isolated(spikes, lfp, refreshcyc=true)
+        using Plot.lfplot
+        _, spikes = Load.register(lfp, spikes; transfer=["phase"], on="time")
+        #phasepl
+        histogram(spikes.time, group=spikes.isolated, normalize=:pdf)
+        histogram(spikes.phase, group=spikes.isolated, normalize=:pdf, alpha=0.33)
+        histogram(spikes.phase, group=spikes.isolated, normalize=:probability, alpha=0.33)
+        dropmissing!(spikes, :isolated)
+    end
 
     shuffle_type = :dotson
     if shuffle_type == :dotson
@@ -116,20 +135,17 @@ F = OrderedDict{NamedTuple, Any}()
     result_dict = OrderedDict{Real, Any}()
     spikes = dropmissing(spikes, :trajreltime)
     @showprogress "widths" for widths ∈ [5f0]
-        @showprogress "Datacut iteration" for datacut ∈ [:all, :cue, :memory,
-                                                         :task, :nontask,
-                                                         :cue_correct,:cue_error,
-                                                         :mem_correct,:mem_error,
-                                                         :correct,:error]
+        @showprogress "Datacut iteration" for datacut ∈ [:all, :cue, :memory, :task, :nontask, :cue_correct,
+                                                         :cue_error, :mem_correct,:mem_error, :correct,:error]
             #[:all, :cue, :memory, :task, :nontask]
             finished_batch = false
             @showprogress "Props" for props ∈ prop_set
                     marginal = get_shortcutnames(props)
-                    key  = get_key(;marginal, datacut, shifts, widths, thresh,
-                                   animal, day)
-                    filt = filts[datacut]
+                    key      = get_key(;marginal, datacut, shifts, widths, thresh,
+                                        animal, day)
                     @info filt filts[datacut]
                     #if keymessage(I, key); continue; end
+                    filt = filts[datacut]
                     @time tmp = Timeshift.shifted_fields(beh, spikes,
                                 shifts, props; fieldpreset=:yartsev,
                                                shiftbeh=false,
@@ -141,8 +157,8 @@ F = OrderedDict{NamedTuple, Any}()
         end
         #Timeshift.save_mains(I)
         #Timeshift.save_mains(F)
-        #
     end
+
 end
 
 savefile = datadir("timeshift","fixed_shifts_$shifts.serial")
