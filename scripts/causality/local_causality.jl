aac813f9e5162f16669e37aa44ae97e1056e7697
Ryan <ryoung@brandeis.edu>
2022-12-04 01:46:17 -0500

    Making local_causality script more flexible. Prepping to try other property combinations.

--- scripts/causality/local_causality.jl
+++ scripts/causality/local_causality.jl
@@ -53,102 +53,127 @@ else
     "$animal$day.$(N)seg"
 end
 
-savefile = datadir("manifold","causal","local_grid_cause_props=$(join(props,","))_$(paramstr)_$tagstr.jld2")
-if isfile(savefile)
-    D=JLD2.load(savefile)
-    Utils.dict.load_dict_to_module!(Main, D)
-end
 
 ## ----------
 ## PARAMETERS
 ## ----------
+
+
+function obtain_local_binned_measure(em, beh, props; grid_kws, savefile,
+        checkpoint=ThreadSafeDict{NamedTuple, Bool}())
+    if isfile(savefile)
+        D=JLD2.load(savefile)
+        Utils.dict.load_dict_to_module!(Main, D)
+    end
+    ## ------------------------------
+    ## Setup frames and zone triggers
+    ## ------------------------------
+    propstime = ["time",props...]
+    EFF = EmbeddingFrameFetch(em, :area, beh, props; 
+                              ordering=Dict(:area=>[:ca1,:pfc]))
+    grd = binning.get_grid(beh, props; grid_kws...)
+    #trig = triggering.get_triggergen(0.5, grid, EFF[4]...)
+
+    ## ------------------------
+    ## Setup stores for results
+    ## ------------------------
+    ca1pfc = fill(NaN,size(grd)..., length(EFF), params[:horizon].stop)
+    pfcca1 = fill(NaN,size(grd)..., length(EFF), params[:horizon].stop)
+    
+
+    ## ------------------------
+    ## Compute
+    ## ------------------------
+    #(e,eff) = first(enumerate(EFF))
+
+    @showprogress "frames" for (e,eff) in enumerate(EFF[1:length(EFF)])
+        @info e
+        zones = triggering.get_triggergen(1, grd, eff...)
+        #(idx,data) = zones[50]
+        Threads.@threads for (idx,data) in zones
+            if isempty(data); continue; end
+            checkpointkey = (;idx, e)
+            if checkpointkey ∈ keys(checkpoint) && checkpoint[checkpointkey]
+                continue
+            else
+                checkpoint[checkpointkey] = false
+            end
+            count = 0
+            cpv = view(ca1pfc, idx..., e, :)
+            pcv = view(pfcca1, idx..., e, :)
+            for D in data
+                if isempty(D); continue; end
+                ca1, pfc = map(d->transpose(hcat(d.data...)), D)
+                try
+                    cpt = causal.predictiveasymmetry(ca1, pfc; params...)
+                    pct = causal.predictiveasymmetry(pfc, ca1; params...)
+                    if all(isnan.(cpv)) && all(isnan.(pcv))
+                        cpv .= 0
+                        pcv .= 0
+                    end
+                    cpv .+= cpt
+                    pcv .+= pct
+                catch err
+                    showerror(stdout, err)
+                    #@infiltrate
+                end
+                count += 1
+            end
+            ca1pfc[idx..., e, :] ./= count
+            pfcca1[idx..., e, :] ./= count
+            checkpoint[checkpointkey] = true
+        end
+        storage    = JLD2.jldopen(savefile, "a")
+        delete!(storage,"ca1pfc")
+        storage["ca1pfc"] = ca1pfc
+        delete!(storage,"pfcca1")
+        storage["pfcca1"] = pfcca1
+        delete!(storage,"checkpoint")
+        storage["checkpoint"] = checkpoint
+        close(storage)
+    end
+
+
+    # Better weighting
+    weighting_trig = zeros(size(grd)..., length(EFF))
+    times = []
+    for (e,eff) in enumerate(EFF)
+        zones = triggering.get_triggergen(1, grd, eff...)
+        #@infiltrate length(zones) == 0
+        for (idx,data) in zones
+            weighting_trig[idx..., e] += length(data)
+            [append!(times, d[1].time) for d in data]
+            #@infiltrate !isempty(data)
+        end
+        # @infiltrate all(weighting_trig[:,:,e] .== 0)
+    end
+    (;ca1pfc, pfcca1, weighting_trig, checkpoint)
+end
+get_savefile(props) = datadir("manifold","causal","local_grid_cause_props=$(join(props,","))_$(paramstr)_$tagstr.jld2")
+
+# =====
+# X - Y
+# =====
 grid_kws=(;widths=[5f0,5f0])
 props = ["x","y"]
-propstime = ["time",props...]
+savefile = get_savefile(props)
+ca1pfc, pfcca1, weighting_trig, checkpoint = 
+        obtain_local_binned_measure(em, beh, props; grid_kws, savefile)
+JLD2.jldsave(savefile; params, est, grd, grid_kws, weighting_trig,
+             props, ca1pfc, pfcca1, checkpoint)
 
-## ------------------------------
-## Setup frames and zone triggers
-## ------------------------------
-EFF = EmbeddingFrameFetch(em, :area, beh, props; 
-                          ordering=Dict(:area=>[:ca1,:pfc]))
-grd = binning.get_grid(beh, props; grid_kws...)
-#trig = triggering.get_triggergen(0.5, grid, EFF[4]...)
+# ========================
+# X - Y - CUEMEM - CORRERR
+# ========================
+props = ["x", "y", "cuemem", "correct"]
+grid_kws=(;widths=[5f0,5f0,1f0,1f0], radiusinc=[1.2f0,1.2f0,1f0,1f0])
+savefile = get_savefile(props)
+ca1pfc, pfcca1, weighting_trig, checkpoint = 
+        obtain_local_binned_measure(em, beh, props; grid_kws, savefile)
 
-## ------------------------
-## Setup stores for results
-## ------------------------
-ca1pfc = fill(NaN,size(grd)..., length(EFF), params[:horizon].stop)
-pfcca1 = fill(NaN,size(grd)..., length(EFF), params[:horizon].stop)
-checkpoint = ThreadSafeDict{NamedTuple, Bool}()
-
-## ------------------------
-## Compute
-## ------------------------
-#(e,eff) = first(enumerate(EFF))
-
-@showprogress "frames" for (e,eff) in enumerate(EFF[1:length(EFF)])
-    @info e
-    zones = triggering.get_triggergen(1, grd, eff...)
-    #(idx,data) = zones[50]
-    Threads.@threads for (idx,data) in zones
-        if isempty(data); continue; end
-        checkpointkey = (;idx, e)
-        if checkpointkey ∈ keys(checkpoint) && checkpoint[checkpointkey]
-            continue
-        else
-            checkpoint[checkpointkey] = false
-        end
-        count = 0
-        cpv = view(ca1pfc, idx..., e, :)
-        pcv = view(pfcca1, idx..., e, :)
-        for D in data
-            if isempty(D); continue; end
-            ca1, pfc = map(d->transpose(hcat(d.data...)), D)
-            try
-                cpt = causal.predictiveasymmetry(ca1, pfc; params...)
-                pct = causal.predictiveasymmetry(pfc, ca1; params...)
-                if all(isnan.(cpv)) && all(isnan.(pcv))
-                    cpv .= 0
-                    pcv .= 0
-                end
-                cpv .+= cpt
-                pcv .+= pct
-            catch err
-                showerror(stdout, err)
-                #@infiltrate
-            end
-            count += 1
-        end
-        ca1pfc[idx..., e, :] ./= count
-        pfcca1[idx..., e, :] ./= count
-        checkpoint[checkpointkey] = true
-    end
-    storage    = JLD2.jldopen(savefile, "a")
-    delete!(storage,"ca1pfc")
-    storage["ca1pfc"] = ca1pfc
-    delete!(storage,"pfcca1")
-    storage["pfcca1"] = pfcca1
-    delete!(storage,"checkpoint")
-    storage["checkpoint"] = checkpoint
-    close(storage)
-end
-
-JLD2.jldsave(savefile; params, est, grd, grid_kws, 
-             props, propstime, ca1pfc, pfcca1, checkpoint)
-
-# Better weighting
-weighting_trig = zeros(size(grd)..., length(EFF))
-times = []
-for (e,eff) in enumerate(EFF)
-    zones = triggering.get_triggergen(1, grd, eff...)
-    #@infiltrate length(zones) == 0
-    for (idx,data) in zones
-        weighting_trig[idx..., e] += length(data)
-        [append!(times, d[1].time) for d in data]
-        #@infiltrate !isempty(data)
-    end
-    # @infiltrate all(weighting_trig[:,:,e] .== 0)
-end
+# ========================
+# X - Y - START - STOP
+# ========================
 
 #                    
 #,---.|         |    
