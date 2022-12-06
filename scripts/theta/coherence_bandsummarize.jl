fbb23a72db148f44d91f436acc486352ae1ee7af
Ryan <ryoung@brandeis.edu>
2022-12-06 14:24:44 -0500

    Coherence at isolated spike times

--- scripts/theta/coherence_bandsummarize.jl
+++ scripts/theta/coherence_bandsummarize.jl
@@ -9,6 +9,11 @@ Load.register(beh, dfa; transfer=["correct","cuemem"], on="time")
 dfa[!,:C] = replace(dfa.C, NaN=>missing)
 dropmissing!(dfa,:C)
 
+function clean_cuecorr(df)
+    df = dropmissing(df, [:cuemem, :correct])
+    @subset(df, vec(any(:cuemem .== [0, 1]',dims=2)) .&& vec(any(:correct .== [0, 1]',dims=2)) .|| 
+            (:cuemem .== -1 .&& :correct .== -1))
+end
 function get_effects(dfa,pos...)
     dfa = !isempty(pos) ? subset(dfa, pos...) : dfa
     groups = groupby(dfa, ["correct","cuemem"])
@@ -20,8 +25,7 @@ function get_effects(dfa,pos...)
                                        :S2=>(x->std(x)./sqrt(length(x)))=>:S2err,
                                       ), 
                                [:cuemem,:correct]), :cuemem)
-    effect = @subset(effect, vec(any(:cuemem .== [0, 1]',dims=2)) .&& vec(any(:correct .== [0, 1]',dims=2)) .|| 
-            (:cuemem .== -1 .&& :correct .== -1))
+    clean_cuecorr(effect)
 end
 
 theta = get_effects(dfa, :freq=>f-> Utils.in_range(f, [6,12]))
@@ -31,4 +35,41 @@ nottheta = get_effects(dfa, :freq=>f-> Utils.not_in_range(f, [4,15]))
 #not_in_theta_harmons = TODO
 overall = get_effects(dfa)
 
+lfp_ca1 = Load.load_lfp(animal,day,tet=:default, subtract_earlytime=true)
+lfp_ca1 = Munge.lfp.annotate_cycles(lfp_ca1)
+spikes = Munge.spiking.isolated(spikes, lfp_ca1, refreshcyc=true)
+dfa_theta  = combine(groupby(subset(dfa,:freq=>f->Utils.in_range(f,[6,12])), :time),
+                     :C=>mean=>:Ctheta,renamecols=false)
+Load.register(dfa_theta, spikes; on="time", transfer=["Ctheta"])
+dfa_delta  = combine(groupby(subset(dfa,:freq=>f->Utils.in_range(f,[1,3])), :time),
+                     :C=>mean=>:Cdelta,renamecols=false)
+Load.register(dfa_delta, spikes; on="time", transfer=["Cdelta"])
+dfa_ripple  = combine(groupby(subset(dfa,:freq=>f->Utils.in_range(f,[150,220])), :time),
+                     :C=>mean=>:Cripple,renamecols=false)
+Load.register(dfa_ripple, spikes; on="time", transfer=["Cripple"])
+Load.register(beh, spikes; on="time", transfer=["cuemem","correct"])
 
+# RESULT :: Isolated spikes more common in high theta coherence
+iso = combine(groupby(spikes, :isolated), 
+        :Ctheta=>c->nanmean(c[(!).(ismissing.(c))]),
+        :Ctheta=>(c->(c=c[(!).(ismissing.(c))];nanstd(c)./length(c)))=>:Ctheta_err,
+        :Cdelta=>c->nanmean(c[(!).(ismissing.(c))]),
+        :Cdelta=>(c->(c=c[(!).(ismissing.(c))];nanstd(c)./length(c)))=>:Cdelta_err,
+        :Cripple=>c->nanmean(c[(!).(ismissing.(c))]),
+        :Cripple=>(c->(c=c[(!).(ismissing.(c))];nanstd(c)./length(c)))=>:Cripple_err,
+        renamecols=false 
+       )
+clean_cuecorr(iso)
+
+# CONTENTION :: Less of an effect when looked at split by task variables 
+#                (task explains more than iso)
+@time isocc = combine(groupby(spikes, [:isolated,:cuemem,:correct]), 
+        :Ctheta=>c->nanmean(c[(!).(ismissing.(c))]),
+        :Ctheta=>(c->(c=c[(!).(ismissing.(c))];nanstd(c)./length(c)))=>:Ctheta_err,
+        :Cdelta=>c->nanmean(c[(!).(ismissing.(c))]),
+        :Cdelta=>(c->(c=c[(!).(ismissing.(c))];nanstd(c)./length(c)))=>:Cdelta_err,
+        :Cripple=>c->nanmean(c[(!).(ismissing.(c))]),
+        :Cripple=>(c->(c=c[(!).(ismissing.(c))];nanstd(c)./length(c)))=>:Cripple_err,
+        renamecols=false 
+       )
+clean_cuecorr(isocc)
