95a2dfd9ad16b169ed19f257e2f86b542679d977
Ryan <ryoung@brandeis.edu>
2023-01-25 01:52:38 -0500

    A few more hatraj behavior plots

--- scripts/behavior/hatraj.jl
+++ scripts/behavior/hatraj.jl
@@ -1,22 +1,49 @@
 using Plots
+import Plot
 
-hatraj  = replace(beh.hatraj, missing=>"")
-uHatraj = unique(skipmissing(beh.hatraj))
+Plot.setparentfolder()
+Plot.setfolder("behavior", "hatraj")
+Plot.setappend("_$animal.$day")
+
+behsort = sort(beh, :hatraj)
+hatraj  = replace(behsort.hatraj, missing=>"")
+uHatraj = unique(skipmissing(behsort.hatraj))
 
 # CUEMEM frequency
-H = []
-for traj in uHatraj
-    inds =  hatraj .== traj
-    push!(H, Plots.histogram(beh[inds, :cuemem], bins=-1.5:0.5:1.5, title=traj, xticks=[-1,0,1]))
+begin
+    H = []
+    for traj in uHatraj
+        inds =  hatraj .== traj
+        push!(H, Plots.histogram(behsort[inds, :cuemem], bins=-1.5:0.5:1.5, title=traj, xticks=[-1,0,1],label=""))
+    end
+    Plots.plot(H..., size=(1400,1400))
+    Plot.save("cuemem frequency")
 end
-Plots.plot(H..., size=(1400,1400))
 
 # CORRECT FREQUENCY
-H = []
-for traj in uHatraj
-    inds =  hatraj .== traj
-    push!(H, Plots.histogram(beh[inds, :correct], bins=-1.5:0.5:1.5, title=traj, xticks=[-1,0,1]))
+begin
+    H = []
+    for traj in uHatraj
+        inds =  hatraj .== traj
+        push!(H, Plots.histogram(behsort[inds, :correct], bins=-1.5:0.5:1.5, title=traj, xticks=[-1,0,1],label=""))
+    end
+    Plots.plot(H..., size=(1400,1400))
+    Plot.save("correct frequency")
 end
-Plots.plot(H..., size=(1400,1400))
 
+begin
+    cm = sort(countmap(beh.hatraj))
+    bar(cm; xticks=(collect(1:length(cm)) .- 0.5, collect(keys(cm))))
 
+    b = subset(beh, :cuemem => c -> c .== 0, :correct => c-> c.==1)
+    cm = sort(countmap(b.hatraj))
+    b1=bar(cm; title="cue correct", xticks=(collect(1:length(cm)) .- 0.5, collect(keys(cm))))
+
+    b = subset(beh, :cuemem => c -> c .== 1, :correct => c -> c .== 1)
+    cm = sort(countmap(b.hatraj))
+    b2=bar(cm; title="mem correct" ,xticks=(collect(1:length(cm)) .- 0.5, collect(keys(cm))))
+
+    plot(b1,b2, layout=grid(2,1))
+
+    Plot.save("By hatraj label, mem and cue correct")
+end
