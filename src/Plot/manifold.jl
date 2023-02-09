b38719af56ac6b7d634a5952ef128dfa7bb8da5e
Ryan <ryoung@brandeis.edu>
2023-02-09 14:02:38 -0500

    updates to manifold recipe capabilities

--- src/Plot/manifold.jl
+++ src/Plot/manifold.jl
@@ -14,23 +14,41 @@ module manifold
     using DIutils
     import DIutils.plotutils: getplotcolor
 
-    # KEEP THIS HERE FOR INTRA-SCRIPT TESTING @userplot ManiPlot
+    # KEEP THISHERE FOR INTRA-SCRIPT TESTING @userplot ManiPlot
+    @userplot ManiPlot
     @recipe function maniplot(plt::ManiPlot; n=14_000, kalpha=0.5, 
-            llim=nothing, origin=0, 
+            llim=nothing, origin=nothing, 
             by=nothing, byaxis=false,
-            quantfilt=0.999,
+            quantfilt=0.99,
             linealpha=nothing, pointalpha=nothing, cmap=:vik)
 
         em = plt.args[1]
-        n = min(n, size(em,1))
+        K = 1
         @assert em isa AbstractArray
         if quantfilt !== nothing
-            qs = [ e .> quantile(e, 1-quantfilt) .&& e .< quantile(e, quantfilt)
-                  for e in eachcol(E) ]
-            qs = accumulate!(.&&, qs)
-            E = E[qs, :]
-            by = by[qs, :]
+            qs = [ (e .> quantile(e, 1-quantfilt) .&& 
+                    e .< quantile(e, quantfilt))
+                  for e in eachcol(em) ]
+            qs = hcat(qs...)
+            @infiltrate
+            qs = findall(vec(all(qs, dims=2)))
+            em = em[qs, :]
+            by = by !== nothing ? by[qs, :] : nothing
+            #@info "size(em) = $(size(em))"
         end
+        if origin !== nothing
+            origin = length(origin) != size(em,2) ?
+                fill(origin, size(em,2)) : origin
+        else
+            origin = median(em, dims=1)
+            @info "median, $origin"
+        end
+        if llim !== nothing
+            llim = length(llim) != size(em,2) ?
+                fill(llim, size(em,2)) : llim
+        end
+
+        n = min(n, size(em,1))
         I = 1:n
         E = em[I,:];
         E = if byaxis 
@@ -45,24 +63,18 @@ module manifold
         else
             E
         end
-
-        @series begin
-            colormap --> cmap
-            width := 10
-            subplot := 2
-            seriestype := :heatmap
-            (fill(NaN,2,2))
-        end
         if llim !== nothing
-            @info llim
-            ylims := (origin-llim, origin+llim)
-            xlims := (origin-llim, origin+llim)
-            zlims := (origin-llim, origin+llim)
+            #@info llim
+            ylims := (origin[1]-llim[1], origin[1]+llim[1])
+            xlims := (origin[2]-llim[2], origin[2]+llim[2])
+            zlims := (origin[3]-llim[3], origin[3]+llim[3])
         end
         @series begin 
             subplot --> 1
             if linealpha !== nothing
                 alpha := linealpha
+            else
+                alpha := kalpha * K
             end
             seriestype := :line
             color := :black
@@ -74,14 +86,14 @@ module manifold
         ylabel --> "y"
         zlabel --> "z"
         colorbar --> true
-        alpha --> 0.1 * kalpha
+        alpha --> kalpha * K
         markersize --> 2
         seriestype := :scatter
         if pointalpha !== nothing
             alpha := pointalpha
         end
         color --> if by !== nothing
-            @info cmap
+            #@info cmap
             getplotcolor(by[I], cmap)
         else
             theme_palette(:auto)[1]
