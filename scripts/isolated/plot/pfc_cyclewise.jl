8b8b8216ff679345d9f7d1fe9cdeff8ba6ddb241
Ryan <ryoung@brandeis.edu>
2023-02-14 02:19:53 -0500

    pfc_cyclewise, fixes for downstream code, nead area in row data

--- scripts/isolated/plot/pfc_cyclewise.jl
+++ scripts/isolated/plot/pfc_cyclewise.jl
@@ -35,74 +35,76 @@ DIutils.filtreg.register(cells, Rdf, on="unit", transfer=["area"])
 
 # Annotate spikes and Rdf
 dropmissing!(Rdf, :isolated_sum)
-Rdf_cycles = groupby(Rdf, :cycle)
+Rdf_cycles = groupby(Rdf, [:cycle, :area])
 Rdf_isocycles = unique(@subset(Rdf, :isolated_sum .> 0).cycle)
+uArea = unique(cells.area)
+indexers = [:time,:isolated_sum]
 
-kws=(;label="")
-plot(
-     (@df cycstat histogram(:isolated_sum ;xlabel="iso spikes emitted", kws...)),
-     (@df cycstat histogram(:isodiversity;xlabel="# of uniq iso cells", kws...)),
-     (@df cycstat histogram(:adjdiversity;ylabel="# of uniq adj cells", kws...)),
-     Plot.blank((plot();Plots.annotate!(0.5,0.5,text("Cycle statistics",14))), visible=false, size=(100,50)),
-     layout=grid(2,2))
+begin
+    kws=(;label="")
+    plot(
+         (@df cycstat histogram(:isolated_sum ;xlabel="iso spikes emitted", kws...)),
+         (@df cycstat histogram(:isodiversity;xlabel="# of uniq iso cells", kws...)),
+         (@df cycstat histogram(:adjdiversity;ylabel="# of uniq adj cells", kws...)),
+         Plot.blank((plot();Plots.annotate!(0.5,0.5,text("Cycle statistics",14))), visible=false, size=(100,50)),
+         layout=grid(2,2))
+end
 
-# Get the firing rates per cycle
-indexers = [:time,:isolated_sum, :area]
-# Dcyc = Dict(
-#             :fr  => [],
-#             :cyc => [],
-#             :relcyc => [],
-#             (x=>[] for x in indexers)...
-#            )
 df, cyc_error = [], Dict()
 begin
 
-    using Infiltrator
     prog = Progress(length(Rdf_isocycles); desc="cycle df")
-    #= Threads.@threads =# for cyc in Rdf_isocycles
+    Threads.@threads for (i,cyc) in collect(enumerate(Rdf_isocycles))
         
         try
              # Address cycles of interest
-             🔑s = [(;cycle=cyc) for cyc in UnitRange(cyc-8, cyc+8)]
+             🔑s = [(;cycle=cyc, area) 
+                    for cyc in UnitRange(cyc-8, cyc+8),
+                    area in uArea
+                   ]
 
             # Grab each cycle of activity
             U = [begin
-                     @infiltrate
                  u = unstack(Rdf_cycles[🔑], indexers, :unit, val)
-                 combine(u, Not(:time) .=> [mean], renamecols=false)
+                 combine(u, Not([:time]) .=> [mean], renamecols=false)
              end
                 for 🔑 in 🔑s if 🔑 in keys(Rdf_cycles)]
-            
-            cycs = hcat(([[🔑.cycle, relcyc] for (🔑,relcyc) in zip(🔑s, UnitRange(-8,8)) 
-                          if 🔑 in keys(Rdf_cycles)])...)'
+
+            cycs = [🔑.cycle for 🔑 in 🔑s 
+                        if 🔑 in keys(Rdf_cycles)]
+            relcycs = [🔑.cycle-cyc for 🔑 in 🔑s 
+                          if 🔑 in keys(Rdf_cycles)]
+            area = [🔑.area for 🔑 in 🔑s 
+                          if 🔑 in keys(Rdf_cycles)]
 
             # Added df to list
-            push!(df, hcat(DataFrame(cycs,[:cycs,:relcycs]), vcat(U...)))
+            push!(df, 
+                  hcat(DataFrame([cycs,relcycs,area],[:cycs,:relcycs,:area]), vcat(U...; cols=:union))
+                 )
             next!(prog)
-            # push!(Dcyc[:fr], U[:, length(indexers)+1:end])
-            # push!(Dcyc[:cyc], cycs)
-            # push!(Dcyc[:relcyc], relcycs)
-            # for indexer in indexers
-            #     push!(Dcyc[indexer], U[:, indexer])
-            # end
         catch exception
             cyc_error[cyc] = exception
+        #     if mod(i, 100) == 0
+        #         @info cyc_error
+        #     end
+            sleep(0.1)
         end
 
     end
+
     @info cyc_error
     df = vcat(df...)
+
 end
 
 # Checkpoint
 fn=replace(path_iso(opt["animal"], opt["day"]),
         ".jld2"=>"_cyclewise.jld2")
-fn = open(fn,"r")
-jldsave(fn; compress=true, df, cyc_error)
+jldsave(fn, true; df, cyc_error)
 
 # Group the dataframe into subdataframes
 # by relative cycle and area
-dfg = groupby(df,  [:relcycs, :area])
+dfg  = groupby(df,  [:relcycs, :area])
 dfgc = groupby(df, [:relcycs, :area, :unit])
 
 # Let
