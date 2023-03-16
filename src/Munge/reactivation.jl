e5bde19ff1dd1ed591a567776f86ba65bd84ddb5
Ryan <ryoung@brandeis.edu>
2023-03-16 19:35:54 -0400

    Reactivation methods prototyped

---
+++ src/Munge/reactivation.jl
@@ -0,0 +1,85 @@
+__precompile__(false)
+"""
+    reactivation.jl
+
+Contains functions for reactivation analysis.
+
+Detecting cell assemblies in large neuronal populations    
+https://www.sciencedirect.com/science/article/pii/S0165027013001489?via%3Dihub
+"""
+module reactivation
+
+    using MultivariateStats
+
+    """
+        zscoreFRmatrix(X::Union{DimArray,Matrix})
+
+    Returns a matrix of z-scores of the columns of X, which
+    are neurons and rows are time bins. 
+    """
+    zscoreFRmatrix(X::Union{DimArray,Matrix}) =
+                            hcat(zscore.(eachcol(X), dims=1)...)
+
+    """
+        function correlationMatrix(zX::Union{DimArray,Matrix},
+                                   zY::Union{DimArray,Matrix}=zX)::Matrix
+
+    Returns the correlation matrix of z-scores of the firing rates of neurons.
+    
+    # Arguments
+    - `zX::Union{DimArray,Matrix}`: Matrix of z-scores of the firing rates of neurons.
+    - `zY::Union{DimArray,Matrix}`: Matrix of z-scores of the firing rates of neurons.
+
+    # Returns
+    - `Matrix`: Correlation matrix.
+    """
+    correlationMatrix(zX::Union{DimArray,Matrix}) = zX' * zX / size(zX, 1)
+    correlationMatrix(zX::Union{DimArray,Matrix},
+                      zY::Union{DimArray,Matrix}) = zX' * zY / size(zX, 1)
+
+    """
+        function getPrecorrelationMatrix_fromICA(X::Union{DimArray,Matrix};
+            maxiter=100, tol=1e-5, do_whiten=true, usefastica=false)::ICA
+
+    Returns the precorrelation matrix from the ICA of X.
+
+    # Arguments
+    - `X::Union{DimArray,Matrix}`: Matrix of z-scores of the firing rates of neurons.
+    - `maxiter::Int`: Maximum number of iterations.
+    - `tol::Float64`: Tolerance.
+    - `do_whiten::Bool`: Whether to whiten the data.
+    - `usefastica::Bool`: Whether to use fastica.
+    
+    # Returns
+    - `ICA`: ICA object.
+    """
+    function getPrecorrelationMatrix_fromICA(X::Union{DimArray,Matrix};
+        maxiter=100, tol=1e-5, do_whiten=true, usefastica=false)::ICA
+
+        ica = ICA()
+        fit!(ica, X, minimum(size(X)) - 1;
+            maxiter, tol, do_whiten)
+        ica
+    end
+
+    """
+        function reactivationScore(Zarea1, W, Zarea2)
+
+    Returns the reactivation score between two areas, Zarea1 and Zarea2,
+
+    Zarea1 and Zarea2 are matrices of z-scores of the firing rates of neurons in
+    area 1 and area 2, respectively. W is the pattern matrix.
+
+    # Arguments
+    - `Zarea1::Matrix`: Matrix of z-scores of the firing rates of neurons in area 1.
+    - `W::Matrix`: Project pattern matrix containing the assemblies
+    - `Zarea2::Matrix`: Matrix of z-scores of the firing rates of neurons in area 2.
+    """
+    function reactivationScore(Zarea1, P::Matrix, Zarea2)
+        Zarea1' * P * Zarea2 / (norm(Zarea1) * norm(Zarea2))
+    end
+    function reactivationScore(Zarea1, P::ICA, Zarea2)
+        reactivationScore(Zarea1, P.W, Zarea2)
+    end
+
+end
