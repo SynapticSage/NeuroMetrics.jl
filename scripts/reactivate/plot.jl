59588528e7efcd4798d0c402b303886d177e9ee2
Ryan <ryoung@brandeis.edu>
2023-04-10 16:08:12 -0400

    Bug sighted: an issue with dfs... `startWell` .== `startWell_tmpl` seems to always be true, which is not desired.

--- scripts/reactivate/plot.jl
+++ scripts/reactivate/plot.jl
@@ -1,21 +1,9 @@
 include("run.jl") # Just so language-server protocol can find the symbols
                   # from the script that generated the data.
 
-# ------------------------------------------------------------
-#  _| || |_  | | | |_   _ _ __   | |_ ___  ___| |_(_)_ __   __ _ 
-# |_  ..  _| | |_| | | | | '_ \  | __/ _ \/ __| __| | '_ \ / _` |
-# |_      _| |  _  | |_| | |_) | | ||  __/\__ \ |_| | | | | (_| |
-#   |_||_|   |_| |_|\__, | .__/   \__\___||___/\__|_|_| |_|\__, |
-#                   |___/|_|                               |___/ 
-# ------------------------------------------------------------
-DF = DIutils.arr.get_quantile_filtered(DF, :value, 0.001)
-# Want to select train=moving and test=stationary
-enforce_conditions = [:moving_tmpl => x -> x .== true,
-                      :moving       => x -> x .== false]
-
-default_match_cols =  [[:startWell, :stopWell, :startstopWell, :ha, :epoch],
-                       [:startWell_tmpl, :stopWell_tmpl, :startstopWell_tmpl, 
-                        :ha_tmpl, :epoch_tmpl]]
+# ------------------------------------------------
+#               HELPER FUNCTIONS
+# ------------------------------------------------
 """
     get_pmatch_cols(decouple_cols)
 Return the columns to partially match the actual labels of the samples (what
@@ -48,11 +36,6 @@ function set_pmatch!(decouple_cols)
                     dims=2) |> vec
     nothing
 end
-DFS.pmatch = all(Matrix(DFS[!, match_cols[1]]) .== Matrix(DFS[!, match_cols[2]]),
-                dims=2) |> vec
-DFS.exclude = DFS.n .< 10
-
-
 """
     subset_dfs(pos...)
 Subset the dataframe `DFS` by the conditions in `general_conditions` and
@@ -79,8 +62,6 @@ function subset_dfs(pos...)
     end
     return dfs_match, dfs_nonmatch
 end
-dfs_match, dfs_nonmatch = subset_dfs()
-
 # ------------------------------------------------
 function difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
     # do matching sets have a higher reactivation score?
@@ -105,52 +86,6 @@ function difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
     ylims!(0, 10000)
     return h
 end
-difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
-# ------------------------------------------------
-
-h = []
-for k in 1:8
-    dfs_match, dfs_nonmatch = subset_dfs(:component => x-> x .== k)
-    hh = difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
-    push!(h, 
-    plot(hh, xlabel="Reactivation score", ylabel="Count"))
-end
-plot(h..., layout=(2,4), size=(1200, 800), 
-title="Reactivation scores for component 1-8")
-
-
-h=[]
-for areas in ["ca1-pfc", "ca1-ca1", "pfc-pfc"]
-    dfs_match, dfs_nonmatch = subset_dfs(:areas => x-> x .== areas)
-    hh=difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
-    push!(h,plot(hh, title="Reactivation scores for matched sets $areas", 
-        xlabel="Reactivation score", ylabel="Count"))
-end
-plot(h..., layout=(1,3), size=(1200, 400))
-
-h=[]
-for epochs in unique(DF.epoch)
-    dfs_match, dfs_nonmatch = subset_dfs(:epoch => x-> x .== epochs)
-    hh=difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
-    push!(h,plot(hh, title="Reactivation scores for matched sets\nepoch=$epochs", 
-                     xlabel="Reactivation score", ylabel="Count"))
-end
-plot(h..., layout=(1,3), size=(1200, 400))
-
-# ------------------------------------------------
-# Same as above section, but split out by different startWell to the
-# same stopWell given (train_startWell, train_stopWell)
-#
-# In other words, we want to collect FOR each matching set (matched 
-# on stopWell, ha, epoch) the reactivation score for each startWell
-#
-# Step 1: Change .pmatch to encode match excepting the startWell
-# Step 2: function that subsets, hypothesis tests to the REPL, and
-#         plots the histogram as above
-# ------------------------------------------------
-DFS.exclude = DFS.startWell .== -1      .|| DFS.stopWell .== -1 .|| 
-              DFS.startWell_tmpl .== -1 .|| DFS.stopWell_tmpl .== -1
-
 """
     match_nonmatch_splits(split_tmpl_match = [:startWell],
                           template_dims = [:startWell_tmpl, :stopWell_tmpl])
@@ -213,9 +148,106 @@ function match_nonmatch_splits(actual_dims   = [:startWell],
     return matching, nonmatching, tmpl_matches
 end
 
+
+# ------------------------------------------------------------
+#  _| || |_  | | | |_   _ _ __   | |_ ___  ___| |_(_)_ __   __ _ 
+# |_  ..  _| | |_| | | | | '_ \  | __/ _ \/ __| __| | '_ \ / _` |
+# |_      _| |  _  | |_| | |_) | | ||  __/\__ \ |_| | | | | (_| |
+#   |_||_|   |_| |_|\__, | .__/   \__\___||___/\__|_|_| |_|\__, |
+#                   |___/|_|                               |___/ 
+# ------------------------------------------------------------
+DF = DIutils.arr.get_quantile_filtered(DF, :value, 0.001)
+# Want to select train=moving and test=stationary
+enforce_conditions = [:moving_tmpl => x -> x .== true,
+                      :moving       => x -> x .== false]
+
+default_match_cols =  [[:startWell, :stopWell, :startstopWell, :ha, :epoch],
+                       [:startWell_tmpl, :stopWell_tmpl, :startstopWell_tmpl, 
+                        :ha_tmpl, :epoch_tmpl]]
+DFS.exclude = DFS.n .< 10
+
+
+dfs_match, dfs_nonmatch = subset_dfs()
+
+difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
+# ------------------------------------------------
+
+h = []
+for k in 1:8
+    dfs_match, dfs_nonmatch = subset_dfs(:component => x-> x .== k)
+    hh = difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
+    push!(h, 
+    plot(hh, xlabel="Reactivation score", ylabel="Count"))
+end
+plot(h..., layout=(2,4), size=(1200, 800), 
+title="Reactivation scores for component 1-8")
+
+
+h=[]
+for areas in ["ca1-pfc", "ca1-ca1", "pfc-pfc"]
+    dfs_match, dfs_nonmatch = subset_dfs(:areas => x-> x .== areas)
+    hh=difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
+    push!(h,plot(hh, title="Reactivation scores for matched sets $areas", 
+        xlabel="Reactivation score", ylabel="Count"))
+end
+plot(h..., layout=(1,3), size=(1200, 400))
+
+h=[]
+for epochs in unique(DF.epoch)
+    dfs_match, dfs_nonmatch = subset_dfs(:epoch => x-> x .== epochs)
+    hh=difference_in_react_match_nonmatch(dfs_match, dfs_nonmatch)
+    push!(h,plot(hh, title="Reactivation scores for matched sets\nepoch=$epochs", 
+                     xlabel="Reactivation score", ylabel="Count"))
+end
+plot(h..., layout=(1,3), size=(1200, 400))
+
+# ------------------------------------------------
+# Same as above section, but split out by different startWell to the
+# same stopWell given (train_startWell, train_stopWell)
+#
+# In other words, we want to collect FOR each matching set (matched 
+# on stopWell, ha, epoch) the reactivation score for each startWell
+#
+# Step 1: Change .pmatch to encode match excepting the startWell
+# Step 2: function that subsets, hypothesis tests to the REPL, and
+#         plots the histogram as above
+# ------------------------------------------------
+DFS.exclude = DFS.startWell      .== -1      .|| DFS.stopWell .== -1 .|| 
+              DFS.startWell_tmpl .== -1 .|| DFS.stopWell_tmpl .== -1
+
+# ISSUE: houston, we have a problem.  why is startwell_tmpl always ==
+#         startwell?  
+#------------------------------------------------
+@assert .!all(DFS.startWell_tmpl .== DFS.startWell)
+@assert .!all(dfs.startWell_tmpl .== DFS.startWell)
+dfss = combine(groupby(dfs, [:startWell_tmpl, :stopWell_tmpl, :startWell]),
+    :mean => mean, :mean => std, :mean => length)
+sort!(dfss, [:stopWell_tmpl, :startWell_tmpl, :startWell, ])
+dfss
+#ISSUE:----------------------------------------------------------------
+
 # Measure for template=[startWell, stopWell] and actual=[startWell]
 matching, nonmatching, tmpl_matches = match_nonmatch_splits()
 print("tmpl_matches: ");
 tmpl_matches
 
-# 
+# Now we want to summarize the effects and plot them out
+tmpl_matches
+μM = map(collect(matching)) do x
+    if x === missing
+        missing
+    else
+        mean(x.mean)
+    end
+end
+μN = map(collect(nonmatching)) do x
+    if x === missing
+        missing
+    else
+        mean(x.mean)
+    end
+end
+
+heatmap.(eachslice(μN, dims=3))
+heatmap.(eachslice(μM, dims=3))
+
